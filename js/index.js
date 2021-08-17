44ca823d1a500ed71b161d78ff4485375c116685
raphabraga <raphael-braga@hotmail.com>
2021-08-17 01:55:45 -0300

    created function that constantly render the elements in the game background (loop), and added a function to give motion to the background screen

--- js/index.js
+++ js/index.js
@@ -1,7 +1,19 @@
+const backgroundMoviment = () => {
+  left = parseInt($("#game-bg").css("background-position"));
+  $("#game-bg").css("background-position", left - 1);
+};
+
+const loop = () => {
+  backgroundMoviment();
+};
+
 function start() {
   $("#game-instructions").hide();
   $("#game-bg").append("<div id='player' class='chopper-animated'></div>");
   $("#game-bg").append("<div id='enemy1' class='chopper-animated'</div>");
   $("#game-bg").append("<div id='enemy2'></div>");
   $("#game-bg").append("<div id='ally' class='ally-animated'></div>");
+
+  var game = {};
+  game.timer = setInterval(loop, 30);
 }
