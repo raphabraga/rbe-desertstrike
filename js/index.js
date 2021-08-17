5814730f59aad3c0a9b00402e529bcdecb4223b6
raphabraga <raphael-braga@hotmail.com>
2021-08-17 02:51:31 -0300

    added motion to the enemy1 element goint in direction to the player until its limits

--- js/index.js
+++ js/index.js
@@ -27,17 +27,32 @@ const movePlayer = (key) => {
   }
 };
 
-const loop = () => {
-  backgroundMoviment();
-};
-
 function start() {
   $("#game-instructions").hide();
   $("#game-bg").append("<div id='player' class='chopper-animated'></div>");
   $("#game-bg").append("<div id='enemy1' class='chopper-animated'</div>");
   $("#game-bg").append("<div id='enemy2'></div>");
   $("#game-bg").append("<div id='ally' class='ally-animated'></div>");
+
+  const moveEnemy1 = () => {
+    let enemy1Distance = parseInt($("#enemy1").css("left"));
+    $("#enemy1").css("left", enemy1Distance - enemy1Speed);
+    $("#enemy1").css("top", enemy1Height);
+    if (enemy1Distance < 0) {
+      enemy1Height = Math.random() * 334;
+      $("#enemy1").css("top", enemy1Height);
+      $("#enemy1").css("left", 694);
+    }
+  };
+
+  const loop = () => {
+    backgroundMoviment();
+    moveEnemy1();
+  };
+
   window.addEventListener("keydown", (event) => movePlayer(event.key));
   var game = {};
+  var enemy1Speed = 5;
+  var enemy1Height = Math.random() * 334;
   game.timer = setInterval(loop, 30);
 }
