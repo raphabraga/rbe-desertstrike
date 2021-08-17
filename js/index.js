776f8acd1d37e86f19009717f1b2341195446315
raphabraga <raphael-braga@hotmail.com>
2021-08-17 02:22:25 -0300

    added key listerner in order to make the player move in the screen respection limits

--- js/index.js
+++ js/index.js
@@ -3,6 +3,30 @@ const backgroundMoviment = () => {
   $("#game-bg").css("background-position", left - 1);
 };
 
+const movePlayer = (key) => {
+  let playerTop = parseInt($("#player").css("top"));
+  switch (key) {
+    case "W":
+      $("#player").css("top", playerTop - 10);
+      if (playerTop < 0) $("#player").css("top", playerTop + 10);
+      break;
+    case "w":
+      $("#player").css("top", playerTop - 10);
+      if (playerTop < 0) $("#player").css("top", playerTop + 10);
+      break;
+    case "S":
+      $("#player").css("top", playerTop + 10);
+      if (playerTop > 434) $("#player").css("top", playerTop - 10);
+      break;
+    case "s":
+      $("#player").css("top", playerTop + 10);
+      if (playerTop > 434) $("#player").css("top", playerTop - 10);
+      break;
+    default:
+      break;
+  }
+};
+
 const loop = () => {
   backgroundMoviment();
 };
@@ -13,7 +37,7 @@ function start() {
   $("#game-bg").append("<div id='enemy1' class='chopper-animated'</div>");
   $("#game-bg").append("<div id='enemy2'></div>");
   $("#game-bg").append("<div id='ally' class='ally-animated'></div>");
-
+  window.addEventListener("keydown", (event) => movePlayer(event.key));
   var game = {};
   game.timer = setInterval(loop, 30);
 }
