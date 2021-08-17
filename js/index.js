08a95d3c45e1a573319b07f3b72bb29d74fc2f63
raphabraga <raphael-braga@hotmail.com>
2021-08-17 19:24:58 -0300

    added the shoot element and given motion to it as the D key is pressed

--- js/index.js
+++ js/index.js
@@ -1,38 +1,48 @@
-const backgroundMoviment = () => {
-  left = parseInt($("#game-bg").css("background-position"));
-  $("#game-bg").css("background-position", left - 1);
-};
-
-const movePlayer = (key) => {
-  let playerTop = parseInt($("#player").css("top"));
-  switch (key) {
-    case "W":
-      $("#player").css("top", playerTop - 10);
-      if (playerTop < 0) $("#player").css("top", playerTop + 10);
-      break;
-    case "w":
-      $("#player").css("top", playerTop - 10);
-      if (playerTop < 0) $("#player").css("top", playerTop + 10);
-      break;
-    case "S":
-      $("#player").css("top", playerTop + 10);
-      if (playerTop > 434) $("#player").css("top", playerTop - 10);
-      break;
-    case "s":
-      $("#player").css("top", playerTop + 10);
-      if (playerTop > 434) $("#player").css("top", playerTop - 10);
-      break;
-    default:
-      break;
-  }
-};
-
 function start() {
-  $("#game-instructions").hide();
-  $("#game-bg").append("<div id='player' class='chopper-animated'></div>");
-  $("#game-bg").append("<div id='enemy1' class='chopper-animated'</div>");
-  $("#game-bg").append("<div id='enemy2'></div>");
-  $("#game-bg").append("<div id='ally' class='ally-animated'></div>");
+  const backgroundMoviment = () => {
+    left = parseInt($("#game-bg").css("background-position"));
+    $("#game-bg").css("background-position", left - 1);
+  };
+
+  const shoot = () => {
+    if (canShoot) {
+      canShoot = false;
+      $("#game-bg").append("<div id='shoot'></div>");
+      playerHeight = parseInt($("#player").css("top"));
+      $("#shoot").css("left", 190);
+      $("#shoot").css("top", playerHeight + 50);
+    }
+  };
+
+  const movePlayer = (key) => {
+    let playerTop = parseInt($("#player").css("top"));
+    switch (key) {
+      case "W":
+        $("#player").css("top", playerTop - 10);
+        if (playerTop < 0) $("#player").css("top", playerTop + 10);
+        break;
+      case "w":
+        $("#player").css("top", playerTop - 10);
+        if (playerTop < 0) $("#player").css("top", playerTop + 10);
+        break;
+      case "S":
+        $("#player").css("top", playerTop + 10);
+        if (playerTop > 434) $("#player").css("top", playerTop - 10);
+        break;
+      case "s":
+        $("#player").css("top", playerTop + 10);
+        if (playerTop > 434) $("#player").css("top", playerTop - 10);
+        break;
+      case "D":
+        shoot();
+        break;
+      case "d":
+        shoot();
+        break;
+      default:
+        break;
+    }
+  };
 
   const moveEnemy1 = () => {
     let enemy1Distance = parseInt($("#enemy1").css("left"));
@@ -61,12 +71,30 @@ function start() {
     }
   };
 
+  const moveBullet = () => {
+    if (!canShoot) {
+      bulletDistance = parseInt($("#shoot").css("left"));
+      if (bulletDistance > 900) {
+        canShoot = true;
+        $("#shoot").remove();
+      }
+      $("#shoot").css("left", bulletDistance + 15);
+    }
+  };
+
   const loop = () => {
     backgroundMoviment();
     moveEnemy1();
     moveEnemy2();
     moveAlly();
+    moveBullet();
   };
+
+  $("#game-instructions").hide();
+  $("#game-bg").append("<div id='player' class='chopper-animated'></div>");
+  $("#game-bg").append("<div id='enemy1' class='chopper-animated'</div>");
+  $("#game-bg").append("<div id='enemy2'></div>");
+  $("#game-bg").append("<div id='ally' class='ally-animated'></div>");
 
   window.addEventListener("keydown", (event) => movePlayer(event.key));
   var game = {};
@@ -74,5 +102,6 @@ function start() {
   var enemy1Height = Math.random() * 334;
   var enemy2Speed = 3;
   var allySpeed = 1;
+  var canShoot = true;
   game.timer = setInterval(loop, 30);
 }
